6a591bc2488438c6feb5d47079cf73fe36106139
Ronak Sharma <rsvk1819@gmail.com>
2024-05-14 21:52:45 +0530

    Footer Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/MusicSchoolTestimonials";
 import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
-
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -15,6 +15,7 @@ export default function Home() {
     <MusicSchoolTestimonials/>
     <UpcomingWebinar/>
     <Instructors/>
+    <Footer/>
    </main>
   );
 }
