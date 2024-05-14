59343e2bedee3c49110c0dd7666c8eaf360878f5
Ronak Sharma <rsvk1819@gmail.com>
2024-05-14 19:58:13 +0530

    Completed Wavy BackGround Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/MusicSchoolTestimonials";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
@@ -9,6 +12,9 @@ export default function Home() {
     <HeroSection/>
     <FeaturedCourses/>
     <WhyChooseUs/>
+    <MusicSchoolTestimonials/>
+    <UpcomingWebinar/>
+    <Instructors/>
    </main>
   );
 }
